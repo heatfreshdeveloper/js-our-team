d3325a987a1d927799b03d9cac0bb86ab4ddb894
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-21 18:53:55 +0200

    Completato lo script js

---
+++ js/script.js
@@ -0,0 +1,65 @@
+/* 
+Per prima cosa suddividiamo il lavoro in più step: 
+1 - CREIAMO UN ARRAY CHE CI PERMETTA DI INSERIRE DEI DATI SPECIFICI (Nome, Ruolo e Foto)
+    - All'interno di questo array creiamo un oggetto che ci permetta di inserire delle proprietà con le rispettive chiavi e valori
+2 - CREIAMO UNA FUNZIONE CHE CI PERMETTA DI STAMPARE I DATI DEI VARI MEMBRI DEL TEAM
+    - A questo punto selezioniamo i vari componenti html del DOM che ci interessano
+    - Stabiliamo se vogliamo usare un INNER HTML + template literal o il metodo CREATE ELEMENT
+    - Aggiungiamo al form precedentemente selezionato un evento in cui al click ci permetta di generare i vari elementi HTML
+*/
+
+
+const teamContainer = document.querySelector(".team-container");
+const addMember = document.getElementById("addMemberButton");
+// const cardImage = document.querySelector("card-image");
+// const cardText = document.querySelector("card-text");
+
+let teamDatabase = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "img/wayne-barnett-founder-ceo.jpg"
+    }
+]
+
+console.log(teamDatabase[0].nome);
+
+function createNewCard() {
+    teamContainer.innerHTML = "";
+
+    for (let i = 0; i < teamDatabase.length; i++) {
+        const cardElement = teamDatabase[i];
+
+        teamContainer.innerHTML += 
+        `<div class="team-card">
+        <div class="card-image">
+          <img
+            src="${cardElement.foto}"
+          />
+        </div>
+        <div class="card-text">
+          <h3>${cardElement.nome}</h3>
+          <p>${cardElement.ruolo}</p>
+        </div>
+         </div>`;
+        
+    }
+
+}
+
+const nameMember = document.getElementById("name").value;
+const role = document.getElementById("role").value;
+const image = document.getElementById("image").value;
+
+addMember.addEventListener("click", function () {
+    teamDatabase.push(
+    {
+      nome: nameMember,
+      ruolo: role,
+      foto: image
+    })
+
+    createNewCard()
+})
+
+
